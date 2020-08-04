6639b68e9ed91649be2d77d1ec480c0fbad1fff5
yuya mistui <copcop77@gmail.com>
2020-08-04 18:33:19 +0900

    20200804_result,detail機能追加

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //モジュールを拡張機能として読み込む
-const APIKEY = '5834f48b60fe0c1b17c346d0390a13e5' ;
+const APIKEY = '3f4023db24db21b98cb10f135f2eb863';
 const search_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";//レストラン検索API
 const review_url = "https://api.gnavi.co.jp/PhotoSearchAPI/v3/";//応援口コミAPI
 let http = require('http');
@@ -32,7 +32,7 @@ function getType(_url) {
 }
 
 //----------ぐるなびAPIにリクエスト
-function get_restdata(Search_req_url){
+function get_restsdata(Search_req_url,is_result){
   return new Promise(function (resolve) {
 
     let api_response = {};
@@ -47,20 +47,24 @@ function get_restdata(Search_req_url){
       apiRes.on('end', function() {
         let search_result = JSON.parse(body_search);
         //---検索結果(ヒット数・現在ページ)をオブジェクトに格納
-        //ヒット数・ページ数
-        api_response.total_hit_count = search_result.total_hit_count;
-        let pages_cnt = Math.floor(search_result.total_hit_count / 10);
-        if(search_result.total_hit_count % 10 != 0){pages_cnt++;}
-        api_response.total_pages =  pages_cnt;
+        if(is_result){
+          //ヒット数・ページ数
+          api_response.total_hit_count = search_result.total_hit_count;
+          let pages_cnt = Math.floor(search_result.total_hit_count / 10);
+          if(search_result.total_hit_count % 10 != 0){pages_cnt++;}
+          api_response.total_pages =  pages_cnt;
 
-        //現在ページ
-        api_response.page_offset = search_result.page_offset;
+          //現在ページ
+          api_response.page_offset = search_result.page_offset;
+        }
 
         //---取得したレストランごとの情報をオブジェクトに格納
         search_result.rest.map( item => {
           let imgs = [];
           let tmp_obj = {};
+          //必要な情報取得取得
           restaurants_id.push(item.id);
+          tmp_obj.shop_id = item.id;
           tmp_obj.name = item.name;
           tmp_obj.tel = item.tel;
           tmp_obj.url = item.url;
@@ -68,6 +72,7 @@ function get_restdata(Search_req_url){
           tmp_obj.opentime = item.opentime;
           tmp_obj.category = item.category;
           tmp_obj.pr = item.pr_short;
+
           //アクセス取得
           access = item.access.line + item.access.station + " ";
           if(item.access.station_exit != ""){access += item.access.station_exit;}
@@ -81,6 +86,7 @@ function get_restdata(Search_req_url){
         });
         //---各レストランについて画像を追加取得
         for(let i = 0;i < restaurants_id.length;i++){
+          let cnt = 0;//取得した写真の数
           let review_params = {
             shop_id: restaurants_id[i],
             keyid: APIKEY
@@ -104,13 +110,19 @@ function get_restdata(Search_req_url){
                 for(let j = 0;j < review_hit_count;j++){
                   if (review_result.response[j].photo.image_url.url_320 != "") {
                     api_response.rest[i].imgs.push(review_result.response[j].photo.image_url.url_320);
+                    cnt++;
                   }
                 }
                 //終了条件(非同期なのでこれを行わないとループ途中で値を渡す)
                 if(i == restaurants_id.length - 1){
                   resolve(api_response);
                 }
+              }else{
+                if(i == restaurants_id.length - 1){
+                  resolve(api_response);
+                }
               }
+
             })
           })
         }
@@ -120,20 +132,19 @@ function get_restdata(Search_req_url){
 }
 
 server.on('request', function(req, res) {
-  console.log(req.method);
   url = req.url;
   let response = "";
   let html_requested = false;
-  console.log(url);
   let data = '';
 
   if(req.method == "POST"){
     req.on('data', function(chunk){data += chunk;})
     .on('end', function() {
-      console.log("posturl: "+url);
+      let is_result = false;//apiリクエストのパラメータをページで分けるため
       //----------POSTメソッドで予想されるリクエスト
       switch (url) {
         case '/result':
+        is_result = true;
         response = fs.readFileSync('./views/result.ejs', 'utf-8');
         break;
         case '/detail':
@@ -141,26 +152,36 @@ server.on('request', function(req, res) {
         break;
         default:
           res.writeHead(404);
-          // res.end("指定されたページは見つかりません");
+          res.end("指定されたページは見つかりません");
       }
+
       //----------POSTされたパラメータを一時的にオブジェクト型に
       let querys;
       let search_params = {};
+      let paramsforDetail = {};//Detail送信用の検索条件情報
+      if(is_result){search_params['offset_page'] = 1;}
       let Search_req_url = '';
       if(Object.keys(data).length){
         querys = data.split("&");
         querys.forEach((element) => {
           let key = element.split("=")[0];
           let val = element.split("=")[1];
-          search_params[key] = val;
+          if(is_result){
+            search_params[key] = val;
+          }
+          else{//detail用の処理
+            if(key == 'id') search_params[key] = val;
+            else paramsforDetail[key] = val;
+          }
         });
         search_params["keyid"] = APIKEY;
-        if(!search_params['offset_page']){search_params['offset_page'] = 1;}
-        if(search_params.freeword != ''){
+        if(search_params.freeword != '' && is_result){
           search_params.freeword = decodeURI(search_params.freeword);
         }
-        search_params["latitude"] = '35.658546';
-        search_params["longitude"] = '139.700450';
+        if(is_result){
+          search_params["latitude"] = '35.658546';
+          search_params["longitude"] = '139.700450';
+        }
         let params_toSearch = Object.keys(search_params).map(function(key) {
           return key + '=' + search_params[key];
         }).join('&');
@@ -168,130 +189,35 @@ server.on('request', function(req, res) {
       }
 
       let out = '';
-      // let api_response = {};
-      // api_response.rest = [];
-      // let restaurants_id = [];
-      //---レストラン検索APIへリクエスト
-      // https.get(Search_req_url, function(apiRes) {
-      //     let body_search = '';
-      //     apiRes.on('data', function(chunk) {
-      //       body_search += chunk;
-      //     });
-      //     apiRes.on('end', function() {
-      //       let search_result = JSON.parse(body_search);
-      //       //---検索結果(ヒット数・現在ページ)をオブジェクトに格納
-      //       //ヒット数・ページ数
-      //       api_response.total_hit_count = search_result.total_hit_count;
-      //       let pages_cnt = Math.floor(search_result.total_hit_count / 10);
-      //       if(search_result.total_hit_count % 10 != 0){pages_cnt++;}
-      //       api_response.total_pages =  pages_cnt;
-      //
-      //       //現在ページ
-      //       api_response.page_offset = search_result.page_offset;
-      //
-      //       //---取得したレストランごとの情報をオブジェクトに格納
-      //       search_result.rest.map( item => {
-      //         let imgs = [];
-      //         let tmp_obj = {};
-      //         restaurants_id.push(item.id);
-      //         tmp_obj.name = item.name;
-      //         tmp_obj.tel = item.tel;
-      //         tmp_obj.url = item.url;
-      //         tmp_obj.address = item.address;
-      //         tmp_obj.opentime = item.opentime;
-      //         tmp_obj.category = item.category;
-      //         tmp_obj.pr = item.pr_short;
-      //         //アクセス取得
-      //         access = item.access.line + item.access.station + " ";
-      //         if(item.access.station_exit != ""){access += item.access.station_exit;}
-      //         if(item.access.walk != ""){access += "徒歩"+item.access.walk+"分";}
-      //         tmp_obj.access = access;
-      //         //画像取得
-      //         if(item.image_url.shop_image1 != ""){imgs.push(item.image_url.shop_image1);}
-      //         if(item.image_url.shop_image2 != ""){imgs.push(item.image_url.shop_image2);}
-      //         tmp_obj.imgs = imgs;
-      //         api_response.rest.push(tmp_obj);
-      //       });
-      //       //---各レストランについて画像を追加取得
-      //       for(let i = 0;i < restaurants_id.length;i++){
-      //         let review_params = {
-      //           shop_id: restaurants_id[i],
-      //           keyid: APIKEY
-      //         };
-      //         let params_toReview = Object.keys(review_params).map(function(key) {
-      //           return key + '=' + review_params[key];
-      //         }).join('&');
-      //         Review_req_url = review_url+'?'+params_toReview;
-      //         //応援口コミAPIへリクエスト
-      //         let review_imgs = [];
-      //         https.get(Review_req_url, function(apiRes) {
-      //           let body_review = '';
-      //           apiRes.on('data', function(chunk) {
-      //             body_review += chunk;
-      //           });
-      //           apiRes.on('end', function() {
-      //             review_imgs = [];
-      //             let review_result = JSON.parse(body_review);
-      //             if(!('gnavi' in review_result)){ //エラー判定
-      //               let review_hit_count = review_result.response.total_hit_count;
-      //               for(let j = 0;j < review_hit_count;j++){
-      //                 if (review_result.response[j].photo.image_url.url_320 != "") {
-      //                   api_response.rest[i].imgs.push(review_result.response[j].photo.image_url.url_320);
-      //                 }
-      //               }
-      //               // console.log(review_imgs);
-      //               // api_response.rest[i].imgs.push(review_imgs);
-      //             }
-      //             out = ejs.render(response, api_response);
-      //             console.log(api_response.rest);
-      //           });
-      //         });
-      //       }
-      //     });
-      //   });
-      api_response = get_restdata(Search_req_url).then(
+      api_response = get_restsdata(Search_req_url,is_result).then(
         function(data){
-          console.log(data.rest);
+          if(is_result){
+            data.offset_page = search_params.offset_page;
+            data.latitude = search_params.latitude;
+            data.longitude = search_params.longitude;
+            data.range = search_params.range;
+            data.freeword = search_params.freeword;
+          }
+          else{
+          data.offset_page = paramsforDetail.offset_page;
+          data.latitude = paramsforDetail.latitude;
+          data.longitude = paramsforDetail.longitude;
+          data.range = paramsforDetail.range;
+          data.freeword = paramsforDetail.freeword;
+          }
           out = ejs.render(response, data);
           res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-          res.write(out);
-          res.end();
+          res.end(out);
         });
-      // out = ejs.render(response, get_restdata(api_response));
-
-        // let out = ejs.render(response, api_response);
-        // res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-        // res.write(out);
-        // res.end();
-
-
-
-
-      //----------出力
-      // let out;
-      // if(html_requested == true){
-      //   out = ejs.render(response, params);
-      //   res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-      // }
-      // else {
-      //   out = fs.readFileSync(response,'utf-8');
-      //   res.writeHead(200, {'Content-Type': getType(response)+'; charset=UTF-8'});
-      // }
-      // res.write(out);
-      // res.end();
     });
   }
-  else if(req.method == "GET"){
 
+  else if(req.method == "GET"){
     switch (url) {
       case '/index':
       case '/':
       html_requested = true;
       response = fs.readFileSync('./views/index.ejs', 'utf-8');
-      break;
-      case '/result':
-      html_requested = true;
-      response = fs.readFileSync('./views/result.ejs', 'utf-8');
       break;
       default:
       url = "./views" + url;
@@ -304,13 +230,18 @@ server.on('request', function(req, res) {
     }
     else {
       out = fs.readFileSync(response,'utf-8');
+      if(getType(response).indexOf('image') != -1){
+        out = fs.readFileSync(response,'binary');
+      }
       res.writeHead(200, {'Content-Type': getType(response)+'; charset=UTF-8'});
     }
-    res.write(out);
-    res.end();
+    if(getType(response).indexOf('image') != -1){
+      res.end(out,'binary');
+    }
+    res.end(out);
   }
 });
 
-server.listen(port, hostname, function() {//サーバ起動時に呼ばれる
+server.listen(port, hostname, function() {
     console.log(`Server runnning at http://${hostname}:${port}/`);
 });
--- views/js/result.js
+++ views/js/result.js
@@ -1,242 +1,49 @@
-//グローバル変数
-let now_page = 1;
-let hit_count = 0;
-let pages_cnt = 0;
-let params = {};
-let keyid = '1dd05f7c051161320f1a936fd5e11ced';
-const rest_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";
-const review_url = "https://api.gnavi.co.jp/PhotoSearchAPI/v3/";
-let restaurants_id= [];
-
-//レストラン画像を口コミAPIにリクエスト、更新
-const getImg = () =>{
-  for(i = 0;i < restaurants_id.length;i++){
-    imageUrl = $('.restaurant-thumb').eq(i).css('background-image');
-    imageUrl = imageUrl.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-    if(imageUrl.match(/sample.png/)){
-      let review_params = {
-        keyid : keyid,
-        shop_id : restaurants_id[i]
-      };
-      let review_img;
-      $.getJSON( review_url, review_params, result => {
-        //非同期処理のためiが正しく取得できないためidの称号でindexを取得
-        idx = $.inArray(review_params.shop_id, restaurants_id);
-        review_hit_count = result.response.total_hit_count;
-        for(j = 0;j<review_hit_count;j++){
-          if (result.response[j].photo.image_url.url_320 != "") {
-            review_img = result.response[j].photo.image_url.url_320;
-            break;
-          }
-        }
-        if(review_img != ""){
-          $('.restaurant-thumb').eq(idx).css('background-image','url('+review_img+')');
-        }
-      });
-    }
-  }
-}
-
-//ページネーション項目クリック時処理
-const updatePage = (input_page,total_pages) =>{
-  $('.page_num').remove();
-  if(total_pages > 5){
-    //最初から1番目or2番目
-    if(input_page < 1 + 2){
-      for(i = 1;i <= 5;i++){
-        $('.pagectrl-nxt').before('<li class="page_num">'+i+'</li>');
-      }
-    }
-    //最後から1番目or2番目
-    else if(input_page > total_pages-2){
-      for(i = total_pages - 4;i <= total_pages;i++){
-        $('.pagectrl-nxt').before('<li class="page_num">'+i+'</li>');
-      }
-    }
-    //選択したページに前後2ページ存在するなら
-    else{
-      for(i = input_page - 2;i <= input_page + 2;i++){
-        $('.pagectrl-nxt').before('<li class="page_num">'+i+'</li>');
-      }
-    }
-  }
-  else{
-    for(i = 1;i <= total_pages;i++){
-      $('.pagectrl-nxt').before('<li class="page_num">'+i+'</li>');
-    }
-  }
-  now_page = input_page;
-  params["offset_page"] = now_page;
-}
-
-// 結果表示
-const showResult = (result) =>{
-  //---ヒット数表示
-  $(".hit-cnt").html('<p class="hit-cnt">全'+hit_count+'件<span>(1～10件)</span></p>');
-
-  //---総ページ数表示
-  pages_cnt = Math.floor(hit_count / 10);
-  if(hit_count % 10 != 0){pages_cnt++;}
-  updatePage(now_page,pages_cnt);
-
-  //---初期化
-  restaurants_id = [];
-  $('.restaurants-container .restaurant').remove();
-
-  //---取得した要素を処理
-  result.rest.map( item => {
-    let imgs = [];
-    restaurants_id.push(item.id);
-
-    access = item.access.line + item.access.station + " ";
-    if(item.access.station_exit != ""){access += item.access.station_exit;}
-    if(item.access.walk != ""){access += "徒歩"+item.access.walk+"分";}
-    if(item.image_url.shop_image1 != ""){imgs.push(item.image_url.shop_image1);}
-    if(item.image_url.shop_image2 != ""){imgs.push(item.image_url.shop_image2);}
-    //---レストランリスト表示用DOM生成
-    let restaurant_DOM = "";
-    restaurant_DOM = '<div class="restaurant"><a href="#" class="restaurant-link"></a>';
-    restaurant_DOM += '<p class="restaurant-cat">'+item.category+'</p>';
-    //サムネイル
-    img_style = "";
-    if(imgs.length > 0){
-      img_style = 'style = \"background-image: url(\''+imgs[0]+'\')\"';
-    }
-    restaurant_DOM += '<div class="restaurant-thumb" '+img_style+'></div>';
-    restaurant_DOM += '<div class="restaurant-inner-detail"><div class="restaurant-title"><h2>'+item.name+'</h2><p class="restaurant-pr"></p></div>';
-
-    //営業時間
-    if(item.opentime != ""){
-      restaurant_DOM += '<div class="restaurant-opentime restaurant-info"><img src="img/clock.png" alt=""><p>'+item.opentime+'</p></div>';
-    }
-    //アクセス
-    if(access != " " && access != ""){
-      restaurant_DOM += '<div class="restaurant-access restaurant-info"><img src="img/train.png" alt=""><p>'+access+'</p></div>';
-    }
-    //住所
-    if(item.address != ""){
-      restaurant_DOM += '<div class="restaurant-address restaurant-info"><img src="img/map.png" alt=""><p>'+item.address+'</p></div>';
-    }
-    restaurant_DOM += '</div></div>';
-    $('.restaurants-container').append(restaurant_DOM);
-  });
-
-  //サムネイルがないレストランは口コミから取得する
-  getImg();
-}
-
 $(function(){
-  //----------クエリパラメータを取得
-  //クエリパラメータを辞書型に格納
-  querys = location.search.slice(1).split("&");
-  querys.forEach((element) => {
-    let key = element.split("=")[0];
-    let val = element.split("=")[1];
-    params[key] = val;
-  });
-  params["offset_page"] = now_page;
-  params["keyid"] = keyid;
-  params["latitude"] = '35.658546';
-  params["longitude"] = '139.700450';
-
-  //----------取得したクエリの処理
-  //freewordをUTF-8にデコード
-  if(params.freeword != ''){
-    params.freeword = decodeURI(params.freeword);
-  }
-  //必須項目(keyid,緯度,経度,検索範囲が取得できたか判定)
-  let error = false;
-  if(!params.keyid || !params.latitude || !params.longitude || !params.range){
-    console.log("error");
-    error = true;
-  }
-  //検索条件表示用
-  range = ""; //デフォルト
-  switch (params["range"]) {
-    case '1':
-      range = 300;
-      break;
-    case '2':
-      range = 500;
-      break;
-    case '3':
-      range = 1000;
-      break;
-    case '4':
-      range = 2000;
-      break;
-    case '5':
-      range = 3000;
-      break;
-    default:
-  }
-
-  //検索条件表示用DOM
-  search_option = "<p class='pupup-query'><span> 現在地</span>[緯度:"+params.latitude+" 経度:"+params.longitude+"] <span> 検索半径</span> ["+range+"m]"+"<br><span> フリーワード </span>["+ decodeURIComponent(params.freeword)+"]<p>";
-  $(search_option).insertBefore('.popup-button');
-
-  //----------APIにリクエスト送信
-  $.getJSON( rest_url, params, result => {
-    hit_count = result.total_hit_count;
-    showResult(result);
-  });
-
-  //ページネーション処理-選択ページに応じてリクエスト再送信
+  //----------ページネーションの実装(隠しフォームでpost)
+  let pages_cnt = parseInt($('.pagecnt').text());
   $(document).on('click','.page_num',function(){
+    //ページナンバー取得
     input_page = $(this).text();
     input_page = parseInt(input_page);
-    $('html, body').animate({scrollTop:0},100);
+    $('.form-offset_page').val(input_page);
+    $('.form-toResult').submit();
 
-    updatePage(input_page,pages_cnt);
-    $("#table tr:not(:first-child)").remove();
-    $.getJSON( rest_url, params, result => {
-      showResult(result);
-    });
   });
-
+  //前ページへ
   $(document).on('click','.pagectrl-pre',function(){
-    input_page = now_page;
+    //.form-offset_pageの初期値はpost受信時のoffset_page
+    input_page = $('.form-offset_page').val();
     if(input_page - 1 >= 1){
       input_page--;
     }
-    $('html, body').animate({scrollTop:0},100);
-    updatePage(input_page,pages_cnt);
-    $("#table tr:not(:first-child)").remove();
-    $.getJSON( rest_url, params, result => {
-      showResult(result);
-    });
+    $('.form-offset_page').val(input_page);
+    $('.form-toResult').submit();
   });
-
+  //先ページへ
   $(document).on('click','.pagectrl-nxt',function(){
-    input_page = now_page;
+    input_page = $('.form-offset_page').val();
     if(input_page + 1 <= pages_cnt){
       input_page++;
     }
-    $('html, body').animate({scrollTop:0},100);
-    updatePage(input_page,pages_cnt);
-    $("#table tr:not(:first-child)").remove();
-    $.getJSON( rest_url, params, result => {
-      showResult(result);
-    });
+    $('.form-offset_page').val(input_page);
+    $('.form-toResult').submit();
   });
 
   //----------詳細ページへのリンク
   $(document).on("click",".restaurant",function(){
-    idx = $('.restaurant').index(this);
-    rest_id = restaurants_id[idx];
-    console.log(rest_id);
-    delete params['offset_page'];
-    params_toDetail = $.param(params);
-    window.location.href = "detail.html"+"?"+params_toDetail+'&shop_id='+rest_id;
+    let rest_id = $(this).children('.shop_id_hidden').text();
+
+    $('.form_shop_id').val(rest_id);
+
+    $('.form-toDetail').submit();
   });
 
   //-----------カテゴリをフリーワードに入れて再検索
   $(document).on("click",".restaurant-cat",function(){
-    delete params['offset_page'];
-    params['freeword'] = $(this).text();
-    params_addCategory = $.param(params);
-    console.log(params);
-    window.location.href = "result.html"+"?"+params_addCategory;
+    $('.form-offset_page').val(1);
+    $('.form-input-freeword').val($(this).text());
+    $('.form-toResult').submit();
+    $('.form-input-freeword').val("");
     return false;
   });
 });
